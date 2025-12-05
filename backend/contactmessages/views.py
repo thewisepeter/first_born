527244a7464ce3fb02fe34345c8d6eeb2a20de30
thewisepeter <rempeterwisdom@gmail.com>
2025-12-05 20:59:29 +0300

    exempt messages from CSRF

--- backend/contactmessages/views.py
+++ backend/contactmessages/views.py
@@ -1,3 +1,5 @@
+from django.views.decorators.csrf import csrf_exempt
+from django.utils.decorators import method_decorator
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
@@ -7,6 +9,7 @@ from .serializer import ContactMessageSerializer, TestimonyMessageSerializer
 from .utils import send_contact_email, send_testimony_email, send_thank_you_email
 
 
+@method_decorator(csrf_exempt, name='dispatch')
 class ContactMessageCreateView(APIView):
     def post(self, request):
         serializer = ContactMessageSerializer(data=request.data)
