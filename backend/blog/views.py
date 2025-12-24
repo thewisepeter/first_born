00c86e672690f65965d8c76a5c709f33730d8229
thewisepeter <rempeterwisdom@gmail.com>
2025-12-24 18:15:57 +0300

    fix: Update TestimonyViewSet to allow public access

--- backend/blog/views.py
+++ backend/blog/views.py
@@ -1,7 +1,9 @@
 from rest_framework import viewsets
+from rest_framework.viewsets import ModelViewSet
 from .models import BlogPost, Article, Testimony, HeroSlide
 from .serializer import BlogPostSerializer, ArticleSerializer, TestimonySerializer, HeroSlideSerializer
 from first_ones_api.base_viewsets import AdminReadOnlyModelViewSet
+from rest_framework.permissions import AllowAny
 
 
 class BlogPostViewSet(AdminReadOnlyModelViewSet):
@@ -12,9 +14,10 @@ class ArticleViewSet(AdminReadOnlyModelViewSet):
     queryset = Article.objects.all().order_by('-date_posted')
     serializer_class = ArticleSerializer
 
-class TestimonyViewSet(AdminReadOnlyModelViewSet):
+class TestimonyViewSet(ModelViewSet):
     queryset = Testimony.objects.all()
     serializer_class = TestimonySerializer
+    permission_classes = [AllowAny]
 
 class HeroSlideViewSet(viewsets.ModelViewSet):
     queryset = HeroSlide.objects.all().order_by("-created_at")
